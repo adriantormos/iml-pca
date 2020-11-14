a5a0c5b56cb76745c697e887cfd4447902d1f9fa
ferranagullolopez <ferranagullolopez@gmail.com>
2020-11-14 16:51:12 +0100

    solves bug in reduced dataset class

--- src/data/types/reduced_dataset.py
+++ src/data/types/reduced_dataset.py
@@ -9,7 +9,7 @@ class ReducedDataset(Dataset):
 
     def __init__(self, config, verbose):
         super(ReducedDataset, self).__init__(config, verbose)
-        self.data = pd.read_csv(config['path'])
+        self.data = pd.read_csv(config['path'], index_col=0)
 
     def get_raw_data(self) -> (np.ndarray, np.ndarray):
         return self.get_preprocessed_data()
@@ -20,7 +20,7 @@ class ReducedDataset(Dataset):
     def get_preprocessed_data(self) -> (np.ndarray, np.ndarray):
         values = self.data.iloc[:,:-1].to_numpy()
         labels = self.data.iloc[:,-1:].to_numpy()
-        return values, labels
+        return values, labels.reshape(labels.shape[0])
 
     def get_preprocessed_dataframe(self) -> pd.DataFrame:
         return self.data
